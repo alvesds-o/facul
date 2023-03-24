3344f287275c5bd249f2b4cd015965f2e7055933
alvesds-o <gabrielalvesdsostudy@gmail.com>
2023-03-24 06:54:57 -0300

    Questão 16 atualizada

--- sl_computacionais/java/Aula03/Questao16.java
+++ sl_computacionais/java/Aula03/Questao16.java
@@ -5,54 +5,29 @@ public class Questao16 {
 
         int num1, num2, num3, num4;
 
+        JOptionPane.showMessageDialog(null, "Digite 3 números em ordem cresce. Depois digite um quarto número de sua preferência");
         num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número"));
         num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número"));
         num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número"));
         num4 = Integer.parseInt(JOptionPane.showInputDialog("Digite o quarto número"));
 
-        if (num1 > num2 && num2 > num3) {
-            maiorQue(num1, num2, num3, num4);
+        numCrescente(num1, num2, num3, num4);
+
+        if (num4 > num3) {
+            numCrescente(num1, num2, num3, num4);
         } 
-        else if(num2 > num1 && num1 > num3){
-            maiorQue(num2, num1, num3, num4);
-        } 
-        else if (num3 > num2 && num2 > num1) {
-            maiorQue(num3, num2, num1, num4);
-        } 
-        else if(num2 > num3 && num3 > num1 ) {
-            maiorQue(num2, num3, num1, num4);
+        else if (num4 > num2) {
+            numCrescente(num1, num2, num4, num3);
         }
-        
-        if (num1 > num2 && num2 > num3) {
-            maiorQue(num1, num2, num3, num4);
-        } 
-        else if(num2 > num1 && num1 > num3){
-            maiorQue(num2, num1, num3, num4);
-        } 
-        else if (num3 > num2 && num2 > num1) {
-            maiorQue(num3, num2, num1, num4);
-        } 
-        else if (num2 > num3 && num3 > num1) {
-            maiorQue(num2, num3, num1, num4);
+        else if (num4 > num1) {
+            numCrescente(num1, num4, num2, num3);
         }
-        else if (num4 > num1 && num4 > num2) {
-            maiorQue(num4, num1, num2, num3);
-        }
-        else if (num1 > num4 && num2 > num4) {
-            maiorQue(num1, num2, num4, num3);
-        }
-        else if (num4 > num1 && num2 > num4) {
-            maiorQue(num2, num4, num1, num3);
-        }
-        else if (num3 > num4 && num2 > num1) {
-            maiorQue(num3, num4, num2, num1);
-        }
-        else if (num1 > num3 && num4 > num2) {
-            maiorQue(num1, num3, num4, num2);
+        else {
+            numCrescente(num4, num1, num2, num3);
         }
     }
 
-    public static void maiorQue(int a, int b, int c, int d) {
+    public static void numCrescente(int a, int b, int c, int d) {
         JOptionPane.showMessageDialog(null, a+","+b+","+c+" e "+d);
     }
 }
